a564bd69d15218a95234057241f643fb4edd9d12
BenjaminLiu <imbenliu@gmail.com>
2018-03-24 21:15:16 +0800

    add Ex1

---
+++ CppTemplate.cpp
@@ -0,0 +1,35 @@
+/*===================================================================================================
+Benjamin's C++ programming template file
+You can ignore this file
+===================================================================================================*/
+
+/*
+Student name: Beier (Benjamin) Liu
+Date:
+Exercise xxx:
+
+Remark:*/
+
+#include <iostream>
+using namespace std;
+
+/*===================================================================================================
+Main program:
+Write comments
+
+Implementations:
+Write comments
+
+File content:
+Write comments
+===================================================================================================*/
+
+int main(){
+	// Exercise xyz
+	// Write comments
+	cout<<"\n====================================Exercise xyz=====================================\n"<<endl;
+	cout<<"Hello World!"<<endl;	
+	myFunction();
+	cout<<"Program pause. Press enter to continue.\n"<<endl;
+	return 0;
+}
---
+++ ex1-Introductioen/HelloWorld.cpp
@@ -0,0 +1,34 @@
+/*
+Student name: Beier (Benjamin) Liu
+Date: 3/24/2018
+Exercise 1: Hello world
+
+Remark:
+None
+*/
+
+
+#include <iostream>
+using namespace std;
+
+/*===================================================================================================
+Main program:
+Exercise 1—Hello world
+Write a simple program that prints the message "Hello world!" (followed by an end of
+line).
+Your program will be graded using two automatic tests, one "permissive", that accepts
+slight variations in the output (spaces and letter case), and another one, strict, that requires
+the output to be strictly identical to the described one (including spaces, end of lines and
+letter case).
+
+Implementations:
+Write comments
+===================================================================================================*/
+
+int main(){
+	// Exercise 1
+	// Write comments
+	cout<<"\n====================================Exercise 1=====================================\n"<<endl;
+	cout<<"Hello World!"<<endl;
+	return 0;
+}
